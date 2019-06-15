b9fb12080b6f8c5872777860f4e6e75dc22f0517
jkdky05 <41238847+jkdky05@users.noreply.github.com>
2019-06-15 19:16:10 -0400

    Create view.sql

---
+++ view.sql
@@ -0,0 +1,8 @@
+CREATE VIEW presen AS 
+SELECT c.id as id_show, p.id as id_presen, c.nombre, p.fecha
+FROM cirqueshow c
+JOIN s_l
+ON s_l.id_show = c.id
+JOIN presenta p
+ON p.id_sl = s_l.id
+WHERE c.id = 1
