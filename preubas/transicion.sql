0ba6b437257079b96e0a5e014757cc94478adcac
jkdky05 <41238847+jkdky05@users.noreply.github.com>
2019-07-09 23:13:29 -0400

    Update transicion.sql

--- preubas/transicion.sql
+++ preubas/transicion.sql
@@ -17,6 +17,41 @@ create table trasicion_ingresos(
   ingresos numeric(9,2)
 );
 
+CREATE TABLE transicion_disciplinaid (
+  id SERIAL primary key,
+  id_disci numeric(4),
+  nombre_disci varchar(32),
+  cantidad_ins numeric(4),
+  year numeric(5),
+  Crecimiento text
+);
+
+-- DISCIPLINA
+INSERT INTO transicion_disciplinaid(id_disci,nombre_disci,cantidad_ins,year,Crecimiento)
+SELECT "Disciplina","Nombre","Cantidad de Inscritos","Año","Crecimiento"
+FROM (
+  SELECT 
+  aspirantes.disci_id as "Disciplina",
+  d.nombre as "Nombre",
+  sum(aspirantes.nume_disci) as "Cantidad de Inscritos",
+  date_part('year',c.hora_in) as "Año",
+  ROUND(100*(SUM(aspirantes.nume_disci)-LAG(SUM(aspirantes.nume_disci),1) OVER (ORDER BY aspirantes.disci_id))
+    / (lag(SUM(aspirantes.nume_disci), 1) over (order by aspirantes.disci_id)),2) || '%' as "Crecimiento"
+
+FROM
+  CalenAudicion c,
+  Disciplina d,
+  (SELECT count(disci) as nume_disci, disci.id as disci_id, Calenda.id as Calenda_id
+   FROM a_a as a, Disciplina as disci, CalenAudicion as Calenda
+  WHERE a.id_CalenAudicion = Calenda.id and a.resulta= TRUE and Calenda.id_disci = disci.id
+  GROUP BY disci.id, Calenda.id
+  ORDER BY disci.id) as aspirantes
+WHERE
+   c.id=aspirantes.Calenda_id and aspirantes.Calenda_id = c.id and d.id=aspirantes.disci_id
+GROUP BY  date_part('year',c.hora_in), aspirantes.disci_id, aspirantes.nume_disci, d.nombre
+ORDER BY aspirantes.disci_id
+) AS DISCIPLINAS;
+
 -- AISTENTES
 INSERT INTO trasicion_asistente (nombre_show,continente,nombre_pais,numero_entradas,year,semestre)
 SELECT show,continente,pais,count,ano,semestre
